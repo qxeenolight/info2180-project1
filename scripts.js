1935f01a02e5f1265a7c7396622b52281b334878
Tiffany Parkinson <banana13.tp@gmail.com>
2022-10-30 19:37:41 -0500

    Updated scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/* Add your JavaScript to this file */
+onload = function() {
+    //variables
+    var btn = document.getElementsByTagName("button")[0];
+    var data = document.getElementsByTagName("input")[0];
+    var msg = document.getElementsByClassName("message")[0];
+    
+    btn.setAttribute("type","button");
+
+    //event listener added to subscribe button to add functionality
+    btn.addEventListener("click", function(e) {
+        e.preventDefault()
+        
+        if (data.value == "") {
+            msg.innerHTML = "Please enter a valid email address";
+        }
+        else {
+            msg.innerHTML = `Thank you! Your email address ${data.value} has been added to our mailing list!`;
+        } 
+    })
+    
+}
